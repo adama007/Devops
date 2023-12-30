f72facbadbfc707bcf683ec174a5610da07f53f2
Adam Chouk <adam.chouk@polytechnicien.tn>
2023-12-30 05:25:44 +0100

    update

--- config/db.js
+++ config/db.js
@@ -1,8 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log('MongoDB connected: ' + conn.connection.host.cyan.underline.bold)
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Remove the useCreateIndex option
+    });
 
-module.exports = connectDB
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
